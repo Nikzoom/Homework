76e8fcaa48f39339cf4138224c29cb1e2b13a7d6
Gghojo <91480253+Nikzoom@users.noreply.github.com>
2025-09-04 22:41:20 +0300

    Add files via upload

--- main-h-2.0/js/main.js
+++ main-h-2.0/js/main.js
@@ -1,11 +1,36 @@
-(function() {
-document.addEventListener ('click', burgerNav)
-function burgerNav(e) {
-    const burgerIcon = e.target.closest('.burger')
-    const burgerNavLink = e.target.closest('.nav__list-link')
-    if (!burgerIcon && !burgerNavLink) return
-    if(!document.body.classList.contains('body--open-menu')) {
-        document.body.classList.add('body--open-menu')
-    } else(document.body.classList.remove('body--open-menu'))
-}
+(function () {
+    document.addEventListener('click', burgerNav)
+    function burgerNav(e) {
+        const burgerIcon = e.target.closest('.burger')
+        const burgerNavLink = e.target.closest('.nav__list-link')
+        if (!burgerIcon && !burgerNavLink) return
+        if (!document.body.classList.contains('body--open-menu')) {
+            document.body.classList.add('body--open-menu')
+        } else (document.body.classList.remove('body--open-menu'))
+    }
+
+    const openButton = document.querySelector('.myinfo__img-button')
+    const closeModalButton = document.querySelector('.modal__close')
+    const modal = document.querySelector('.modal')
+    const body = document.body
+
+
+
+    openButton.addEventListener('click', () => {
+        document.body.classList.add('body--opened-modal')
+    })
+
+    modal.addEventListener('click', closeModal)
+    function closeModal(e) {
+        if (e.target.closest('.modal__close') || e.target === modal) {
+            e.preventDefault()
+            body.classList.remove('body--opened-modal')
+        }
+        document.addEventListener('keydown', event => {
+            if (event.code === 'Escape') {
+                body.classList.remove('body--opened-modal')
+            }
+        })
+    }
+
 })()
